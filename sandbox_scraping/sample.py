d5ad995f7d671aa1eac611c690e23c89efba33b4
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-01-10 18:54:13 +0900

    ok

--- sandbox_scraping/sample.py
+++ sandbox_scraping/sample.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+# 参考サイト - qiita @matsu0228
+# python3でwebスクレイピング(Beautiful Soup)
+# https://qiita.com/matsu0228/items/edf7dbba9b0b0246ef8f
+
+
 target_url = 'https://google.co.jp'
 r = requests.get(target_url)  # requestsを使って、webから取得
 soup = BeautifulSoup(r.text, 'lxml')  # 要素を抽出
